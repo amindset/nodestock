1777d089a6e3176626ad6d2a4994de07fc93f571
Steve Harris <harriss305@gmail.com>
2022-12-22 06:47:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const otherstuff = "Hello there, this is other stuff!";
+
+// Set handlebar routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Creates the port for the app
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
